3ae879295d08e841cd6a8279c8a1e9b4bb65ca72
amsully <sullivan1513@gmail.com>
2016-02-26 14:42:00 -0500

    solved russian dolls

--- CompetitiveProgramming_IndependentStudy/src/week/three/RussianDolls_10549.java
+++ CompetitiveProgramming_IndependentStudy/src/week/three/RussianDolls_10549.java
@@ -1,153 +1,166 @@
 package week.three;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class RussianDolls_10549 {
 
-        public static void main(String[] args) throws IOException {
+        static class RussianDoll {
+                int height, diameter, width;
 
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-                String numberOfDolls;
-
-                LinkedList<RussianDoll> dolls = new LinkedList<RussianDoll>();
-
-                while (true) {
-
-                        dolls.clear();
-                        
-                        numberOfDolls = br.readLine();
-                        int setSize = (Integer.parseInt(numberOfDolls));
-                        System.out.println(numberOfDolls);
-                        if (numberOfDolls.equals("0"))
-                                break;
-
-                        int numDolls = Integer.parseInt(numberOfDolls);
-
-                        int totDolls = numDolls * 2;
-
-                        for (int i = 0; i < totDolls; i++) {
-
-                                StringTokenizer tk = new StringTokenizer(br.readLine());
-
-                                int h = Integer.parseInt(tk.nextToken());
-                                int d = Integer.parseInt(tk.nextToken());
-                                int w = Integer.parseInt(tk.nextToken());
-
-                                dolls.add(new RussianDoll(h, d, w));
-
-                        }
-                        
-                        Collections.sort(dolls);
-
-                        for(RussianDoll d: dolls) d.print();
-
-                        
-                        LinkedList<RussianDoll> set1 = new LinkedList<RussianDoll>();
-                        
-                        set1.add(dolls.remove());// Append smallest doll to list.
-                        
-                        Iterator<RussianDoll> itr = dolls.iterator();
-                        
-                        
-                        
-                        RussianDoll curr;
-                        while(itr.hasNext()){
-                                
-                                curr = itr.next();
-                                
-                                curr.print();
-                                set1.getLast().print();
-                                System.out.println( curr.compareTo(set1.getLast() ) );
-                                
-                                if(curr.compareTo( set1.getLast()) > 0){
-                                        set1.add(curr);
-                                        itr.remove();
-                                }
-                                
-                                if( set1.size() == setSize ) break;
-                                 
-                        }
-                        
-                        Comparator<RussianDoll> c = new Comparator<RussianDoll>() {
-
-                                @Override
-                                public int compare(RussianDoll o1, RussianDoll o2) {
-                                        // TODO Auto-generated method stub
-                                        return o2.height - o1.height ;
-                                }
-                                
-                                
-                        };
-                        
-                        Collections.sort(dolls, c);
-                        Collections.sort(set1, c);
-                    
-                        System.out.println("OUTPUT");
-                        for(RussianDoll d: dolls) d.print();
-                        System.out.println("-");
-                        for(RussianDoll d : set1) d.print();
-
+                public RussianDoll(int h, int d, int w) {
+                        height = h;
+                        diameter = d;
+                        width = w;
                 }
 
+                boolean fit(RussianDoll rd) {
+                        int diamterFit = rd.diameter - 2 * rd.width;
+                        int heightFit = rd.height - 2 * rd.width;
 
+                        return (heightFit >= height) && (diamterFit >= diamterFit);
+                }
+
+                public String toString() {
+                        return height + " " + diameter + " " + width;
+                }
 
         }
 
-}
+        static RussianDoll[] dolls;
+        static boolean[] visited;
+        static Stack<RussianDoll> topologicalSort;
+        static byte[][][] dp;
+        static int[][][] next;
+        static int n;
 
-class RussianDoll implements Comparable<RussianDoll> {
+        public static void depthFirstSearch(int i) {
+                if (!visited[i]) {
+                        visited[i] = true;
 
-        int height;
-        int width;
-        int diameter;
+                        for (int j = 0; j < dolls.length; j++) {
+                                if (dolls[i].fit(dolls[j])) {
+                                        depthFirstSearch(j);
+                                }
+                        }
+                        topologicalSort.push(dolls[i]);
 
-        int diameterFit;
-        int heightFit;
-
-        RussianDoll(int h, int d, int w) {
-                height = h;
-                width = w;
-                diameter = d;
-                diameterFit = d - (2 * w);
-                heightFit = h - (2 * w);
+                }
         }
 
-        void printFit() {
-                System.out.println("Height: " + heightFit + " Width: " + diameterFit);
-        }
-        
-        void print(){
-                System.out.println(height + " " + diameter + " " + width);
-        }
-
-//        public int compareTo(RussianDoll rd) {
-//
-//                if (this.diameterFit == rd.diameterFit && this.heightFit == rd.heightFit) {
-//                        return 0;
-//                }
-//                if (this.diameterFit < rd.diameterFit && this.heightFit < rd.heightFit) {
-//                        return -1;
-//                } else {
-//                        return 1;
-//                }
-//        }
-        
-        public int compareTo(RussianDoll rd) {
-
-                if (this.diameterFit < rd.diameter && this.heightFit < rd.height) {
-                        return -1;
-                } else {
+        public static byte run(int i, int j, int ci) {
+                if(dp[i+1][j+1][ci] != -1){
+                        return dp[i+1][j+1][ci];
+                }
+                
+                int nxt = Math.max(i, j) +1;
+                if(nxt  == n  && ci ==n /2){
                         return 1;
+                }else if( nxt  == n)
+                        return 0;
+                
+                byte res= 0;
+                int cj = nxt  - ci;
+                
+                if(ci < n /2 && i == -1 || (dolls[i].fit(dolls[nxt ]))){
+                        res = run(nxt , j, ci+1);
                 }
+                if(res == 1){
+                        next[i+1][j+1][ci] = 0;
+                        return dp[i+1][j+1][ci] =res;
+                }
+                
+                if (cj < n / 2 && j == -1 || (dolls[j].fit(dolls[nxt])))
+                        res = run(i, nxt, ci);
+                if( res == 1){
+                        next[i+1][j+1][ci] = 1;
+                        return dp[i+1][j+1][ci] = 1;
+                }
+                
+                return dp[i + 1][j + 1][ci];
         }
-        
+
+        public static void main(String[] args) throws Exception{
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                
+                int c = 0; 
+                
+                while(true){
+                        n = 2 * Integer.parseInt(br.readLine());
+                        
+                        if(n == 0) break;
+                        if(c != 0) System.out.println();
+                        
+                        c = 1;
+                        
+                        dolls = new RussianDoll[n];
+                        
+                        visited = new boolean[n];
+                        
+                        topologicalSort = new Stack<RussianDoll>();
+
+                        for(int i = 0; i < n; i ++){
+                                String input = br.readLine();
+                                String[] vals = input.split(" " );
+                                
+                                dolls[i] = new RussianDoll(Integer.parseInt(vals[0]), Integer.parseInt(vals[1]), Integer.parseInt(vals[2]));                                
+                        }
+                        
+                        for(int i = 0; i < n; i++){
+                                depthFirstSearch(i);
+                        }
+                        
+                        for(int i = 0; i < dolls.length; i++){
+                                dolls[i] = topologicalSort.pop();
+                        }
+                        
+                        dp = new byte[n+2][n+2][n+1];
+                        next = new int[n+2][n+2][n+1];
+                        
+                        for(int i =0; i < dp.length; i++){
+                                for(int j = 0; j < dp.length; j++){
+                                        for(int k = 0; k < n; k++){
+                                                dp[i][j][k] = -1;
+                                        }
+                                }
+                        }
+                        
+                        Stack<RussianDoll> first = new Stack<RussianDoll>();
+                        Stack<RussianDoll> second = new Stack<RussianDoll>();
+                        
+                        run(-1,-1,0);
+                        
+                        int i = -1;
+                        int j = -1; 
+                        int ni = 0;
+                        
+                        while(true){
+                                int nx = Math.max(i, j) +1;
+                                
+                                if(nx ==n) break;
+                                if(next[i+1][j+1][ni] == 0){
+                                        i = nx;
+                                        ni++;
+                                        first.push(dolls[nx]);
+                                }else{
+                                        j = nx; 
+                                        second.push(dolls[nx]);
+                                }
+                        }
+                        
+                        while(!first.isEmpty()){
+                                System.out.println(first.pop());
+                        }
+                        System.out.println("-");
+                        while(!second.isEmpty()){
+                                System.out.println(second.pop());
+                        }
+                }
+                
+
+                
+        }
+
 }
